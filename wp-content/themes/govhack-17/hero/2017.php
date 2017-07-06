8e63cbb9ceb8224966d8c6eb58dfd932c40b62a0
Andrea Epifani <andrea@andreaepifani.com>
2017-07-06 21:39:49 +1000

    2017 hero fix

--- wp-content/themes/govhack-17/hero/2017.php
+++ wp-content/themes/govhack-17/hero/2017.php
@@ -22,5 +22,41 @@
                 <?php gh_row_close() ?>
             </div>
         </div>
+        <div>
+            <div class="wrapper">
+                <?php gh_row_open() ?>
+                <div class="<?= gh_col_class(1, 6) ?>">
+                    <img class="gh-2016-review-coverpage"
+                         src="<?= esc_url(home_url('/wp-content/uploads/2016/12/2016reviewscreenshot.jpg')) ?>"
+                         alt="2016 Review cover page">
+                </div><!-- gh_col_close -->
+                <div class="<?= gh_col_class(1, 3) ?>">
+                    <div class="hero-inner">
+                        <h1>2016 Review</h1>
+                        <p>Download, read and share our <br>published 2016 Review!</p>
+                        <a class="button-minimal"
+                           href="<?= esc_url(home_url('/wp-content/uploads/2016/11/GovHack-2016-Review.pdf')) ?>">Get
+                            your copy here</a>
+                    </div>
+                </div><!-- gh_col_close -->
+                <div class="<?= gh_col_class(1, 2) ?>">
+                    <div class="hero-inner">
+                        <h1>2017 Volunteers</h1>
+                        <p>Register as a volunteer to direct and run GovHack events!</p>
+                        <p>
+                            <a class="button-minimal" href="//govhack.org/go">
+                                <span class="dashicons dashicons-testimonial"></span> Express interest
+                            </a>
+                        </p>
+                        <div>
+                            <a class="button gh-pink" style="color: white;" href="//govhack.org/signup">
+                                <span class="dashicons dashicons-email"></span> Get our newsletter
+                            </a>
+                        </div>
+                    </div>
+                </div><!-- gh_col_close -->
+                <?php gh_row_close() ?>
+            </div>
+        </div>
     </div>
 </div>
