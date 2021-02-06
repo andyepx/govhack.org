240959a5d62e80651a6ddc04b8fe77091406e093
Debian <bitnami@ip-172-31-1-50.ap-southeast-2.compute.internal>
2021-02-06 00:37:30 +0000

    functions

--- function.php
+++ function.php
@@ -1,9 +1,17 @@
 <PHP
 // GovHack Wordpress Theme
 
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( 'twenty-twenty-one-style' ), 
+        wp_get_theme()->get('Version')
+    );
+}
+
 
 function my_plugin_add_stylesheet() {
-    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', false, '1.0', 'all' );
 }
 
 
